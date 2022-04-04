7cfcb2b717117d7130c85dcf3197b80e2a5952d6
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-04-04 12:10:43 +0800

    Example Script cleanup

--- example_scripts/licence_test_api.py
+++ example_scripts/licence_test_api.py
@@ -3,8 +3,7 @@ import requests
 #status = cancelled, active
 #licence_type =  1=Licence  2=Authorised User  3=Annual Admission
 
-#api_key='5WEEW0D4Q9I7K0XN2Y2Y83RE45HJ66BLYSQ5ZBU866W9CZ3SPY4IPZJQHU0AKQQD6GSG6Z069QOAACFVPBF2SYFOXGZJIOOC301W'
-api_key='5G9BIPB17RS7A4WCG33CWAFXI1P39S93BPN61YMZTZV17F6D86XUX9QRDW7ICXQB8A0M1MQM8A92YWC79ZDTPGOKSO0IXIIREMR4'
+api_key='<api_key>'
 url = 'https://mooring-api-dev-oim01.dbca.wa.gov.au/api/external/licence-create-update/'+api_key+'/'
 myobj = {'vessel_rego': 'D8888','licence_id': 1235, 'licence_type': 2, 'start_date': '2021-08-21','expiry_date' : '2022-05-20', 'status' : 'active'}
 resp = requests.post(url, data = myobj)
--- example_scripts/vessel_test_api.py
+++ example_scripts/vessel_test_api.py
@@ -1,7 +1,6 @@
 import requests
 
-#api_key='5WEEW0D4Q9I7K0XN2Y2Y83RE45HJ66BLYSQ5ZBU866W9CZ3SPY4IPZJQHU0AKQQD6GSG6Z069QOAACFVPBF2SYFOXGZJIOOC301W'
-api_key='5G9BIPB17RS7A4WCG33CWAFXI1P39S93BPN61YMZTZV17F6D86XUX9QRDW7ICXQB8A0M1MQM8A92YWC79ZDTPGOKSO0IXIIREMR4'
+api_key='<api_key>'
 #url = 'http://172.17.0.3:9002//api/external/all-mooring/'+api_key+'/?mooring_specification=private'
 url = 'https://mooring-api-dev-oim01.dbca.wa.gov.au/api/external/vessel-create-update/'+api_key+'/'
 myobj = {'rego_no': 'D8888','vessel_size': 1.1, 'vessel_draft': 1.2, 'vessel_beam': 1.3, 'vessel_weight' : '1.4',}
