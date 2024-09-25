ab99de1c7a25bb47c529cec9edf8831442c6b4aa
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2024-09-25 09:46:46 +0800

    Add the code to display all the tables used in the project

--- mooring/wsgi.py
+++ mooring/wsgi.py
@@ -14,3 +14,16 @@ os.environ.setdefault("BASE_DIR", BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mooring.settings")
 #application = get_wsgi_application()
 application = Cling(MediaCling(get_wsgi_application()))
+
+
+
+# from django.apps import apps
+# from django.db import connection
+
+# all_apps = apps.get_app_configs()
+
+# for app in all_apps:
+#     print(f"App: {app.label}")
+#     for model in app.get_models():
+#         table_name = model._meta.db_table
+#         print(f"  Table: {table_name}")
