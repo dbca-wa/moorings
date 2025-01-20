0aa275ebf14c28ec6feda7fff2f417cd40bf7b12
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-01-20 16:00:16 +0800

    Make all the foreignkey fields to AUTH_USER_MODEL raw_id_fields for quick response

--- mooring/admin.py
+++ mooring/admin.py
@@ -238,12 +238,6 @@ class BookingAdmin(admin.ModelAdmin):
     search_fields = ('customer','id','admission_payment','cost_total')
     list_filter = ('booking_type',)
     readonly_fields=('created','property_cache',)
-    exclude = (
-        # 'customer',
-        # 'overridden_by',
-        # 'created_by',
-        # 'canceled_by',
-    )
 
     def has_add_permission(self, request, obj=None):
         return False
@@ -251,6 +245,7 @@ class BookingAdmin(admin.ModelAdmin):
 @admin.register(models.BookingAnnualAdmission)
 class BookingAnnualAdmissionAdmin(admin.ModelAdmin):
     # raw_id_fields = ('customer','created_by','overridden_by','canceled_by',)
+    raw_id_fields = ('customer','created_by','overridden_by','canceled_by',)
     list_display = ('id','customer','start_dt','expiry_dt','rego_no','booking_type','cost_total','created_by','created')
     ordering = ('-id',)
     search_fields = ('customer__first_name','customer__last_name','id',)
@@ -524,7 +519,7 @@ class AdmissionLineInline(admin.TabularInline):
 
 @admin.register(models.AdmissionsBooking)
 class AdmissionBooking(admin.ModelAdmin):
-    # raw_id_fields = ('customer',)
+    raw_id_fields = ('customer', 'created_by', 'canceled_by')
     list_display = ('confirmation_number', 'booking_type','customer','mobile', 'totalCost','created')
     readonly_fields=('created_by','canceled_by',)
     inlines = [AdmissionLineInline]
