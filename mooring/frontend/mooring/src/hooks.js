b6d8bcdfabb155abe968b1ed8945b0129d6761e1
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-06-16 15:46:01 +0800

    Updated code to align with changes in moment-range usage after the upgrade.

--- mooring/frontend/mooring/src/hooks.js
+++ mooring/frontend/mooring/src/hooks.js
@@ -1,19 +1,19 @@
 // module for all third party dependencies
 
-import $ from 'jquery'
+import $, { extend } from 'jquery'
 import DataTable from 'datatables.net';
 import DataTableBs from 'datatables.net-bs';
 import DataTableRes from 'datatables.net-responsive-bs';
 import bootstrap from 'bootstrap';
 import moment from 'moment/moment.js';
-import MomentRange from 'moment-range';
+import { extendMoment } from 'moment-range';
 import datetimepicker from 'eonasdan-bootstrap-datetimepicker';
 import validate from 'jquery-validation';
 import select2 from 'select2';
 import awesomplete from 'awesomplete';
 import daterangepicker from 'bootstrap-daterangepicker';
 import formValidate from './components/utils/validator.js';
-var Moment = MomentRange.extendMoment(moment);
+var Moment = extendMoment(moment);
 import swal from 'sweetalert2';
 import api_endpoints from './apps/api.js';
 import store from './apps/store';
