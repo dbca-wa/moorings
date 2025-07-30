ac212478680c9496f3d9abc34a901298391316e1
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-07-30 12:51:07 +0800

    Add vue.config.js file to work with VueCLI

---
+++ mooring/frontend/availability2/vue.config.js
@@ -0,0 +1,100 @@
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+const webpack = require('webpack');
+// const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+let port = process.env.PORT ? parseInt(process.env.PORT) : 8082;
+
+module.exports = defineConfig({
+    runtimeCompiler: true,
+    outputDir: path.resolve(__dirname, '../../static/availability2'),
+    publicPath: '/static/availability2/',
+    filenameHashing: false,
+    chainWebpack: (config) => {
+        config.resolve.alias.set(
+            '@vue-utils',
+            path.resolve(__dirname, 'src/utils/vue')
+        );
+        config.resolve.alias.set(
+            '@common-utils',
+            path.resolve(__dirname, 'src/components/common/')
+        );
+        config.resolve.alias.set(
+            '@static-root',
+            path.resolve(__dirname, '../../../staticfiles/')
+        );
+    },
+    configureWebpack: {
+        entry: './src/main.js',
+        devtool: 'source-map',
+        resolve: {
+            fallback: {
+                buffer: require.resolve('buffer/'),
+                stream: false,
+                os: false
+            },
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                moment: 'moment',
+                swal: 'sweetalert2',
+                _: 'lodash',
+            }),
+            // new MomentLocalesPlugin(),
+            new webpack.ProvidePlugin({
+                Buffer: ['buffer', 'Buffer'],
+            }),
+        ],
+        devServer: {
+            host: '0.0.0.0',
+            allowedHosts: 'all',
+            port: port,
+            devMiddleware: {
+                // Save file to disk so the Django server can serve them
+                writeToDisk: true,
+            },
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers':
+                    'Origin, X-Requested-With, Content-Type, Accept',
+            },
+            client: {
+                webSocketURL: 'ws://0.0.0.0:' + port + '/ws',
+            },
+            proxy: {
+                '/api': {
+                    target: 'http://127.0.0.1:9071',
+                    changeOrigin: true,
+                },
+                '/admin': {
+                    target: 'http://127.0.0.1:9071',
+                    changeOrigin: true,
+                }
+            },
+        },
+        module: {
+            rules: [
+                /* config.module.rule('images') */
+                {
+                    test: /\.(png|jpe?g|gif|webp|avif)(\?.*)?$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'img/[name][ext]',
+                    },
+                },
+                /* config.module.rule('fonts') */
+                {
+                    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'fonts/[name][ext]',
+                    },
+                },
+            ],
+        },
+        performance: {
+            hints: false,
+        },
+    },
+});
