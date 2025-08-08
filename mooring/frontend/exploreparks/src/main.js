92388b89aa2c5e6490121844ce46a766ea9c20a4
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-08-08 12:02:16 +0800

    Update main.js to work with Vite

--- mooring/frontend/exploreparks/src/main.js
+++ mooring/frontend/exploreparks/src/main.js
@@ -1,5 +1,19 @@
+import 'vite/modulepreload-polyfill';
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+import $ from 'jquery';
+// import _ from 'lodash';
+import moment from 'moment';
+// import select2 from 'select2';
+import swal from 'sweetalert2';
+
+window.$ = $;
+window.jQuery = $;
+// window._ = _;
+window.moment = moment;
+window.swal = swal;
+
 import './foundation-min.scss';
 import 'foundation-datepicker/css/foundation-datepicker.css';
 import 'ol/ol.css';
