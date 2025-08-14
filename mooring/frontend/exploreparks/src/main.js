521c196f8a762c908c46abd16c07719ca95ed245
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-08-14 10:10:47 +0800

    Remove unused lines

--- mooring/frontend/exploreparks/src/main.js
+++ mooring/frontend/exploreparks/src/main.js
@@ -3,14 +3,11 @@ import 'vite/modulepreload-polyfill';
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import $ from 'jquery';
-// import _ from 'lodash';
 import moment from 'moment';
-// import select2 from 'select2';
 import swal from 'sweetalert2';
 
 window.$ = $;
 window.jQuery = $;
-// window._ = _;
 window.moment = moment;
 window.swal = swal;
 
@@ -19,19 +16,8 @@ import 'foundation-datepicker/css/foundation-datepicker.css';
 import 'ol/ol.css';
 import 'awesomplete/awesomplete.css';
 
-// import Vue from 'vue';
 import { createApp } from 'vue';
-// import VuePaginate from 'vue-paginate';
 import ParkFinder from './parkfinder.vue';
 
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap'
-
-// require('custom-event-polyfill');
-// require('ie-array-find-polyfill');
-
-// Vue.use(VuePaginate);
 const app = createApp(ParkFinder)
 app.mount('#parkfinder')
-
-// global.parkfinder = app
