e0d11b0a4caf2bce7f03f141f13d6b3eaf2b9b97
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-08-15 09:04:27 +0800

    Introduce Vite

---
+++ mooring/frontend/mooring/vite.config.js
@@ -0,0 +1,67 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import svgLoader from 'vite-svg-loader';
+
+const applicationNameShort = 'moorings';
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+const host = process.env.HOST || '0.0.0.0';
+
+export default defineConfig(({ mode }) => {
+    const isProduction = mode === 'production';
+
+    return {
+        base: `/static/${applicationNameShort}_vue/`,  //Base public path when served in development or production.
+        server: {
+            host: host,
+            port: port,
+            strictPort: true,
+            open: false,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers':
+                    'Origin, X-Requested-With, Content-Type, Accept',
+            },
+            origin: `http://localhost:${port}`,
+            hmr: {
+                protocol: 'ws',
+                host: 'localhost',
+                port: port,
+            },
+        },
+        plugins: [
+            vueDevTools(),
+            vue(),
+            svgLoader({
+                defaultImport: 'url',
+            }),
+        ],
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.esm-bundler.js',
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        define: {
+            'process.env': {}
+        },
+        build: {
+            manifest: 'manifest.json',
+            commonjsOptions: { transformMixedEsModules: true },
+            root: path.resolve(__dirname, './src'),
+            outDir: path.resolve(
+                __dirname,
+                `../../static/${applicationNameShort}_vue`
+            ),
+            sourcemap: true,
+            rollupOptions: {
+                input: {
+                    main: path.resolve(__dirname, 'src/apps/main.js'),
+                },
+            },
+            exclude: ['jquery', 'bootstrap'],
+            emptyOutDir: true,
+        },
+    };
+});
