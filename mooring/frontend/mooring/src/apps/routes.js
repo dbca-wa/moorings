6ec693462b6587b8e1c431d116c84e9bb6a3943e
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-08-20 08:59:28 +0800

    ReFormat a file

--- mooring/frontend/mooring/src/apps/routes.js
+++ mooring/frontend/mooring/src/apps/routes.js
@@ -83,7 +83,7 @@ export const routes = [
                     },
                 ]
             },
-                {
+            {
                 path:'bookings',
                 component: BookingIndex,
                 children:[
