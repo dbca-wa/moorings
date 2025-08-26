77af668e79ccc566092dfe5cde96afa8ae7dfcca
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-08-26 09:02:21 +0800

    Remove unused lines

--- mooring/frontend/admissions/src/priceHistory/priceHistory.vue
+++ mooring/frontend/admissions/src/priceHistory/priceHistory.vue
@@ -9,16 +9,13 @@
             <button iiivshow="showAddBtn" @click="showHistory()" class="btn btn-primary pull-right table_btn">Add Price History</button>
         </div>
         <datatable ref="history_dt" :dtHeaders ="dt_headers" :dtOptions="dt_options" id="ph_table"></datatable>
-    <!-- <confirmbox id="deleteHistory" :options="deleteHistoryPrompt"></confirmbox> -->
 </div>
 </template>
 
 <script>
 import datatable from '@/utils/datatable.vue'
-// import confirmbox from '../utils/confirmbox.vue'
 import PriceHistoryDetail from '@/priceHistory/priceHistoryDetail.vue'
 import parkPriceHistory from '@/priceHistory/parkPriceHistoryDetail.vue'
-// import {bus} from '../utils/eventBus.js'
 import {
     $,
     Moment,
@@ -99,22 +96,6 @@ export default {
                 comments:'',
                 reason:{id:1}
             },
-            // deleteHistory: null,
-            // deleteHistoryPrompt: {
-            //     icon: "<i class='fa fa-exclamation-triangle fa-2x text-danger' aria-hidden='true'></i>",
-            //     message: "Are you sure you want to Delete this Price History Record",
-            //     buttons: [{
-            //         text: "Delete",
-            //         event: "delete",
-            //         bsColor: "btn-danger",
-            //         handler: function() {
-            //             vm.deleteHistoryRecord(vm.deleteHistory);
-            //             vm.deleteHistory = null;
-            //         },
-            //         autoclose: true,
-            //     }],
-            //     id: 'deleteHistory'
-            // },
         }
     },
     methods: {
@@ -192,10 +173,7 @@ export default {
         },
         addParkPriceHistory: function() {
             var data = this.validateNewPrice(this.parkPrice)
-            console.log(data)
             var start = this.parkPrice.period_start.split("-");
-            console.log(start)
-            // this.parkPrice.period_start = start[2] + "-" + start[1] + "-" + start[0];
             this.parkPrice.period_start = start[0] + "-" + start[1] + "-" + start[2];
             console.log(this.parkPrice.period_start)
             if (this.parkPrice.period_end){
@@ -301,7 +279,6 @@ export default {
                 else{
                     deleteHistory = $(btn).data('rate');
                 }
-                // bus.emit('showAlert', 'deleteHistory');
                 swal.fire({
                     title: 'Are you sure you want to Delete this Price History Record',
                     text: "You won't be able to revert this!",
