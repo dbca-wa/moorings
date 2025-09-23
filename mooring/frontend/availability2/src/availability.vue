7c129098c4b72931fa95ad00c72afc800b3dc8ef
shibaken@gmail.com <shibaken@gmail.com>
2025-09-23 15:17:31 +0800

    Align "Time Left" and "Cancel" buttons adjacently

--- mooring/frontend/availability2/src/availability.vue
+++ mooring/frontend/availability2/src/availability.vue
@@ -45,11 +45,11 @@
             </div>
 
             <div v-show="ongoing_booking" class="mb-3">
-                <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
                     <!-- Time Left Button -->
                     <button v-show="ongoing_booking" class="btn btn-danger" type="button">Time Left {{ timeleft }} to complete booking.</button>
                     <!-- Cancel Button -->
-                    <a :href="parkstayUrl+'/booking/abort'" class="btn btn-warning">Cancel in-progress booking</a>
+                    <a :href="parkstayUrl+'/booking/abort'" class="btn btn-warning ms-2">Cancel in-progress booking</a>
                 </div>
             </div>
 
