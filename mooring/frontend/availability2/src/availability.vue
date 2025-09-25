0f468330b211e0fd6381f40d4af318d3a88e674e
shibaken@gmail.com <shibaken@gmail.com>
2025-09-25 15:32:23 +0800

    Improve usability of "selected" state in availability calendar

--- mooring/frontend/availability2/src/availability.vue
+++ mooring/frontend/availability2/src/availability.vue
@@ -381,14 +381,15 @@
                                                         <div v-else-if="bp.status == 'selected'">
                                                             <!-- Wrap button and close icon in a relative position container -->
                                                             <div class="position-relative">
-                                                                <button class="btn btn-sm btn-info w-100" @click="deleteBooking(bp.booking_row_id, bp.past_booking)"> 
+                                                                <!-- <button class="btn btn-sm btn-info w-100" @click="deleteBooking(bp.booking_row_id, bp.past_booking)">  -->
+                                                                <button class="btn btn-sm btn-success w-100 disabled" style="pointer-events: none;"> 
                                                                     Book {{ bp.period_name }}
                                                                     <span v-if="site.mooring_class == 'small'">${{ bp.small_price }}</span>
                                                                     <span v-if="site.mooring_class == 'medium'">${{ bp.medium_price }}</span>
                                                                     <span v-if="site.mooring_class == 'large'">${{ bp.large_price }} </span>
                                                                 </button>
                                                                 <!-- Position the close button on top of the main button -->
-                                                                <a href="#" v-show="bp.past_booking == false" class="text-danger position-absolute top-0 end-0" style="transform: translate(6px, -6px); z-index: 5;" @click.prevent.stop="deleteBooking(bp.booking_row_id, bp.past_booking)" title="Remove">
+                                                                <a href="#" v-show="bp.past_booking == false" class="text-danger position-absolute top-0 end-0" style="transform: translate(6px, -6px); z-index: 5;" @click.prevent="deleteBooking(bp.booking_row_id, bp.past_booking)" title="Remove">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                                                                         <circle cx="8" cy="8" r="8" fill="white"/>
                                                                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
