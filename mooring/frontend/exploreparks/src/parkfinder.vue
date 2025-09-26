4217a49fd99d59547f7e20fbc2bb54841db402e1
shibaken@gmail.com <shibaken@gmail.com>
2025-09-26 09:29:08 +0800

    Remove gap between 'Time Left' and Trolley

--- mooring/frontend/exploreparks/src/parkfinder.vue
+++ mooring/frontend/exploreparks/src/parkfinder.vue
@@ -9,7 +9,7 @@
                     <!-- The content of the original left column will go here -->
                     <div v-show="current_booking.length > 0">
                         <!-- Button group using Flexbox for alignment -->
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-3">
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
                             <div class="text-danger d-flex flex-column align-items-center me-3">
                                 <span class="text-danger d-flex align-items-center me-3">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill me-1" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/></svg>
