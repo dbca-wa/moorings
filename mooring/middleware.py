39bd223eea693f61e2dbbce4ff140a4d86ebf408
shibaken@gmail.com <shibaken@gmail.com>
2026-07-27 15:14:03 +0800

    Remove ad_booking session handling from BookingTimerMiddleware
    - Delete the if 'ad_booking' in session block (validation + timeout refresh)
    - Remove ('ad_booking' not in request.session) clause from checkout redirect guard
    - Fix copy-paste error in annual_admission_booking exception comment
    - AdmissionsBooking payment flow is now stateless; no session to validate

--- mooring/middleware.py
+++ mooring/middleware.py
@@ -142,30 +142,12 @@ class BookingTimerMiddleware(object):
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         logger.info("in BookingTimerMiddleware.process_view()...")
-        if 'ad_booking' in request.session:
-            logger.info(f"session['ad_booking']: [{request.session['ad_booking']}] exists.")
-            try:
-                booking = AdmissionsBooking.objects.get(pk=request.session['ad_booking'])
-            except:
-                # no idea what object is in self.request.session['ad_booking'], ditch it
-                del request.session['ad_booking']
-                return
-            # Note: Don't delete session key based on booking_type here.
-            # Views are responsible for session cleanup after successful completion.
-            # booking_type changes from 3->1 during payment, but session key needed for success view.
-            if CHECKOUT_PATH.match(request.path) and request.method == 'POST':
-                # safeguard against e.g. part 1 of the multipart checkout confirmation process passing, then part 2 timing out.
-                # on POST boosts remaining time to at least 2 minutes
-                booking.save()
-        else:
-            logger.info('session[ad_booking] does not exist.')
-
         if 'annual_admission_booking' in request.session:
             logger.info(f"session['annual_admission_booking']: [{request.session['annual_admission_booking']}] exists.")
             try:
                 booking = BookingAnnualAdmission.objects.get(pk=request.session['annual_admission_booking'])
             except:
-                # no idea what object is in self.request.session['ad_booking'], ditch it
+                # no idea what object is in self.request.session['annual_admission_booking'], ditch it
                 del request.session['annual_admission_booking']
                 return
             # Note: Don't delete session key based on booking_type here.
@@ -240,7 +222,7 @@ class BookingTimerMiddleware(object):
         # Note: 'payment_session' is set by create_basket_session/create_checkout_session in the
         # stateless payment flow (no ps_booking). Allow access to /ledger-api/* when it is present.
         if ('ps_booking_internal' not in request.COOKIES) and CHECKOUT_PATH.match(request.path):
-            if ('ps_booking' not in request.session) and CHECKOUT_PATH.match(request.path) and ('ad_booking' not in request.session) and ('annual_admission_booking' not in request.session) and ('payment_session' not in request.session):
+            if ('ps_booking' not in request.session) and CHECKOUT_PATH.match(request.path) and ('annual_admission_booking' not in request.session) and ('payment_session' not in request.session):
                 # return HttpResponseRedirect(reverse('public_make_booking'))
                 url_redirect = reverse('public_make_booking')
                 response = HttpResponse("<script> window.location='"+url_redirect+"';</script> <center><div class='container'><div class='alert alert-primary' role='alert'><a href='"+url_redirect+"'> Redirecting please wait: "+url_redirect+"</a><div></div></center>")
